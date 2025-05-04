886354dd47c53016e3144f7bbe0cef1012ec6481
alexxtos <if23b232@technikum-wien.at>
2025-05-04 19:21:12 +0200

    Implemented TourListView and Navbar with Tailwind, create reusable Add/Edit/Delete buttons with command bindings, integrated pop up window, used replayCommand with ICommand interface for event handling in viewmodels, updated project reference

---
+++ TourPlanner.UI/ViewModels/RelayCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TourPlanner.UI.ViewModels
+{
+    public class RelayCommand : ICommand
+    {
+        // Action to execute when the command is triggered
+        // Function that determines if the command can be executed
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        // 2 Constructors 
+        public RelayCommand(Action execute) : this(execute, null) { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+            {
+                throw new ArgumentNullException(nameof(execute));
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+            _canExecute = canExecute;
+        }
+
+        // CanExecute state might change
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute();
+
+        public void Execute(object parameter) => _execute();
+    }
+}
---
+++ TourPlanner.UI/ViewModels/TourActionButtonsViewModel.cs
@@ -0,0 +1,27 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TourPlanner.UI.Views.Components;
+
+namespace TourPlanner.UI.ViewModels
+{
+    public class TourActionButtonsViewModel
+    {
+        public ICommand AddTourCommand { get; set; }
+
+        public TourActionButtonsViewModel()
+        {
+            AddTourCommand = new RelayCommand(OpenAddTourWindow);
+        }
+
+        private void OpenAddTourWindow()
+        {
+            var dialog = new AddTourDialogView();
+            dialog.ShowDialog(); 
+        }
+    }
+}
