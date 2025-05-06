139f9b6f5e9b6b9520dd8338751a5d0814c3be34
David Nikolic <davidnikolic2308@gmail.com>
2025-05-06 22:11:19 +0200

    Implemented Enums; Added Properties to TourLog-Object

--- TourPlanner.BL/Models/TourLog.cs
+++ TourPlanner.BL/Models/TourLog.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace TourPlanner.BL.Models
 {
-    internal class TourLog
+    public class TourLog
     {
+        public Guid Id { get; set; }
+
+        public Guid TourId { get; set; }
+
+
+        public DateTime TimeStamp { get; set; }
+
+        public string Comment { get; set; }
+
+        public Difficulty Difficulty { get; set; }
+
+        public double Distance { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public SatisfactionRating SatisfactionRating { get; set; }
     }
+}
+
+public enum Difficulty
+{
+    VeryEasy,
+    Easy,
+    Medium,
+    Hard,
+    VeryHard,
+    Impossible
+}
+
+public enum SatisfactionRating
+{
+    VeryDissatisfied,
+    Dissatisfied,
+    Neutral,
+    Satisfied,
+    VerySatisfied
 }
