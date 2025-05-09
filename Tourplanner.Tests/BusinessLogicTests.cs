09d0edb4ffd684143dda695e696dd0f60e16e3f6
David Nikolic <davidnikolic2308@gmail.com>
2025-05-09 14:37:12 +0200

    Removed Models

---
+++ Tourplanner.Tests/BusinessLogicTests.cs
@@ -0,0 +1,22 @@
+﻿using TourPlanner.BL.Models;
+
+namespace Tourplanner.Tests
+{
+    public class BusinessLogicTests
+    {
+
+        Tour t = new();
+
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Console.WriteLine(t);
+        }
+    }
+}
