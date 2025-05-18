12304c02720c9bf8fe721e01f9e456b18ad4978d
David Nikolic <davidnikolic2308@gmail.com>
2025-05-18 17:15:02 +0200

    TourLogService-Interface implemented

--- TourPlanner.BL/Interfaces/ITourLogService.cs
+++ TourPlanner.BL/Interfaces/ITourLogService.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TourPlanner.BL.Models;
 
 namespace TourPlanner.BL.Interfaces
 {
     internal interface ITourLogService
     {
+        void AddTourLog(TourLog tourLog);
+
+        void DeleteTourLog(TourLog tourLog);
+        List<TourLog> GetTourLogs();
+
+        void UpdateTourLog(TourLog tourLog);
     }
 }
