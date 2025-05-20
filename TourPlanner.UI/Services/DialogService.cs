e021a32572c41c856c163d8eadefea3ad2b90108
David Nikolic <davidnikolic2308@gmail.com>
2025-05-20 15:21:52 +0200

    Service functionality implemented

---
+++ TourPlanner.UI/Services/DialogService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.BL.DTOs;
+using TourPlanner.UI.Interfaces;
+using TourPlanner.UI.ViewModels;
+using TourPlanner.UI.Views.Components;
+
+namespace TourPlanner.UI.Services
+{
+    internal class DialogService : IDialogService
+    {
+        public TourLogDTO? DisplayTourLogPopUp(string title, TourLogDTO tourLog = null)
+        {
+            var dialog = new TourLogDialogWindowView();
+            var vm = new TourLogDialogViewModel(title, tourLog);
+
+            dialog.DataContext = vm;
+
+            vm.CloseRequested += () => dialog.DialogResult = true;
+
+            if (dialog.ShowDialog() == true)
+            {
+                return vm.Result;
+            }
+
+            return null;
+        }
+
+        public TourDTO? DisplayTourPopUp(string title, TourDTO tour = null)
+        {
+            var dialog = new TourDialogWindowView();
+            var vm = new TourDialogViewModel(title, tour);
+
+            dialog.DataContext = vm;
+
+            vm.CloseRequested += () => dialog.DialogResult = true;
+
+            if (dialog.ShowDialog() == true)
+            {
+                return vm.Result;
+            }
+
+            return null;
+        }
+    }
+}
