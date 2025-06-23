d87d94e3d47ebfb66cbbec5ced709967b164b242
David Nikolic <davidnikolic2308@gmail.com>
2025-06-23 17:57:23 +0200

    Added Statistics-Object and corresponding service

---
+++ TourPlanner.BL/DTOs/TourStatisticsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TourPlanner.BL.DTOs
+{
+    public class TourStatisticsDTO
+    {
+        public string TourName { get; set; }
+        public int TourId { get; set; }
+        public float AvgDifficulty { get; set; }
+        public float AvgRating { get; set; }
+        public int Popularity { get; set; }
+        public bool IsChildFriendly { get; set; }
+    }
+}
---
+++ TourPlanner.BL/Services/TourStatisticsService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TourPlanner.BL.DTOs;
+
+namespace TourPlanner.BL.Services
+{
+    public class TourStatisticsService
+    {
+        public TourStatisticsDTO CalculateTourStatistic(TourDTO tour, IEnumerable<TourLogDTO> logs)
+        {
+            return new TourStatisticsDTO
+            {
+                TourId = tour.Id,
+                TourName = tour.Name,
+                Popularity = logs.Count(),
+                AvgDifficulty = (float)logs.Average(l => (int)l.Difficulty),
+                AvgRating = (float)logs.Average(l => (int)l.Rating),
+                IsChildFriendly = logs.Average(l => (int)l.Difficulty) <= 1.5 && logs.Average(l => (int)l.Rating) >= 1.5
+            };
+        }
+    }
+}
