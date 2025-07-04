a73a53eafc32080828905882899e54631741e1f5
David Nikolic <davidnikolic2308@gmail.com>
2025-07-04 11:31:39 +0200

    Interface in Mock-Implementation implemented

--- TourPlanner.Tests/Mocks/MockRepos/MockTourLogsRepo.cs
+++ TourPlanner.Tests/Mocks/MockRepos/MockTourLogsRepo.cs
@@ -3,10 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TourPlanner.DAL.Entities;
+using TourPlanner.DAL.Repositories.Interfaces;
 
 namespace TourPlanner.BL.MockRepos
 {
-    public class MockTourLogsRepo
+    public class MockTourLogsRepo : ITourLogRepository
     {
+        public void AddTourLog(TourLogEntity log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddTourLogAsync(TourLogEntity log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteTourLog(int logId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteTourLogAsync(int logId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TourLogEntity> FindTourLogsByFullText(string searchTerm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TourLogEntity> GetAllTourLogs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TourLogEntity> GetAllTourLogsForTour(int tourId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TourLogEntity GetTourLogById(int tourLogId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateTourLog(TourLogEntity log)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTourLogAsync(TourLogEntity log)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
